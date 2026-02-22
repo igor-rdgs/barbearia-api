8eb468abb55ce3b142b8dc31a620f6dd9bf9b923
Igor Rodrigues <igor.aguiar1@live.com>
2026-02-22 19:57:31 -0300

    alterando variáveis do nodemailer

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,7 +1,8 @@
 import nodemailer from "nodemailer";
 
 export const mailer = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
